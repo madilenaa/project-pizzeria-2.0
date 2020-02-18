b42571acc9567a212607e22cda20def759afd946
Magdalena Marchewka <madilenaa@gmail.com>
2020-02-18 23:33:00 +0100

    prepare code to make class AmountWidget

--- project-pizzeria/project-pizzeria/src/js/script.js
+++ project-pizzeria/project-pizzeria/src/js/script.js
@@ -93,11 +93,11 @@
 
       /* find the clickable trigger (the element that should react to clicking) */
       const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-      console.log(clickableTrigger);
+      //console.log(clickableTrigger);
 
       /* START: click event listener to trigger */
       clickableTrigger.addEventListener('click', function(event) {
-        console.log('clicked');
+        //console.log('clicked');
 
         /* prevent default action for event */
         event.preventDefault();
@@ -107,7 +107,7 @@
 
         /* find all active products */
         const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
-        console.log('active Products:', activeProducts);
+        //console.log('active Products:', activeProducts);
 
         /* START LOOP: for each active product */
         for (let activeProduct of activeProducts){
@@ -126,7 +126,7 @@
 
     initOrderForm(){
       const thisProduct = this;
-      console.log('initOrderForm:', thisProduct);
+      //console.log('initOrderForm:', thisProduct);
 
       thisProduct.form.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -143,29 +143,29 @@
         event.preventDefault();
         thisProduct.processOrder();
       });
-      console.log('initOrderForm:', thisProduct);
+      //console.log('initOrderForm:', thisProduct);
     }
 
     processOrder() {
       const thisProduct = this;
-      console.log('processOrder:', thisProduct);
+      //console.log('processOrder:', thisProduct);
 
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
+      //console.log('formData', formData);
 
       // wrote to variable price default price from thisProduct.data.price
       let price = thisProduct.data.price;
-      console.log('price:', price);
+      //console.log('price:', price);
 
       // start loop for params
       for (let paramId in thisProduct.data.params) {
         const param = thisProduct.data.params[paramId];
-        console.log('param:', param);
+        //console.log('param:', param);
 
         //start loop for every options param
         for (let optionId in param.options) {
           const option = param.options[optionId];
-          console.log('option:', option);
+          //console.log('option:', option);
 
           // if option ISN'T default, price is up
           if (formData.hasOwnProperty(paramId) && formData[paramId].includes(optionId) && !option.default) {
@@ -178,7 +178,7 @@
 
           // make constant where are elements
           const optionImages = thisProduct.imageWrapper.querySelectorAll('.' + paramId + '-' + optionId);
-          console.log('images:', optionImages );
+          //console.log('images:', optionImages );
 
           //if option is "check"  - add classNames.menuProduct.imageVisible
           if (formData.hasOwnProperty(paramId) && formData[paramId].includes(optionId)) {
@@ -199,10 +199,20 @@
     }
   }
 
+  class AmountWidget {
+    constructor (element) {
+      const thisWidget = this;
+
+      console.log('AmountWidget:', thisWidget);
+      console.log('constructor arguments:', element);
+    }
+  }
+
+
   const app = {
     initMenu: function() {
       const thisApp = this;
-      console.log('thisApp.data', thisApp.data);
+      //console.log('thisApp.data', thisApp.data);
 
       for(let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);

ec6a212dd896c3eba14055c3dbb2f660bba9bded
Magdalena Marchewka <madilenaa@gmail.com>
2020-03-16 15:51:14 +0100

    Inheritance of classes - workshop video

---
+++ project-pizzeria/project-pizzeria/src/js/components/BaseWidget.js
@@ -0,0 +1,66 @@
+import {select, templates} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+
+class BaseWidget {
+  constructor(wrapperElement, initialValue) {
+    const thisWidget = this;
+
+    thisWidget.dom = {};
+    thisWidget.dom.wrapper = wrapperElement;
+
+    thisWidget.correctValue = initialValue;
+  }
+
+  get value() {
+    const thisWidget = this;
+
+    return thisWidget.correctValue;
+  }
+
+  set value(value) {
+    const thisWidget = this;
+
+    const newValue = thisWidget.parseValue(value);
+
+    /* TO DO: Add validation */
+    if (newValue != thisWidget.correctValue && thisWidget.isValid(newValue)) {
+
+      thisWidget.correctValue = newValue;
+      this.announce();
+    }
+    thisWidget.renderValue();
+  }
+
+  setValue(value) {
+    const thisWidget = this;
+
+    thisWidget.value = value;
+  }
+
+  parseValue(value){
+    const thisWidget = this;
+    return parseInt(value);
+  }
+
+  isValid(value) {
+    const thisWidget = this;
+    return !isNaN(value);
+  }
+
+  renderValue() {
+    const thisWidget = this;
+
+    thisWidget.dom.wrapper.innerHTML = thisWidget.value;
+  }
+
+  announce() {
+    const thisWidget = this;
+
+    const event = new CustomEvent('updated', {
+      bubbles: true
+    });
+    thisWidget.dom.wrapper.dispatchEvent(event);
+  }
+
+}
+export default BaseWidget;
